5ae1548535c30bcf0a323a6fbb04367d463f6bc5
Ian Miller 🦐 <ianmiller347@gmail.com>
2022-12-04 21:28:41 -0500

    make Page component, delete Homepage boilerplate

---
+++ components/Page/Page.tsx
@@ -0,0 +1,32 @@
+import Head from 'next/head';
+import { ReactElement } from 'react';
+import classNames from 'classnames/bind';
+import styles from './Page.module.css';
+
+const cx = classNames.bind(styles);
+
+interface PageProps {
+  children: ReactElement;
+  description: string;
+  title: string;
+}
+
+const Page = ({ children, description, title }: PageProps) => (
+  <div className={cx('container')}>
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <main className={cx('main')}>
+      {children}
+    </main>
+
+    <footer className={cx('footer')}>
+      The Posting Platform
+    </footer>
+  </div>
+)
+
+export default Page;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,13 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Page from '../components/Page'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>The Posting Platform</title>
-        <meta name="description" content="The Posts Platform helps you build a platform for users to create content." />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          The Posting Platform
-        </h1>
-
-        <p className={styles.description}>
+    <Page title="The Posting Platform" description="">
+      <>
+        <p>
           Learn more
         </p>
-      </main>
-
-      <footer className={styles.footer}>
-        The Posting Platform
-      </footer>
-    </div>
+      </>
+    </Page>
   )
 }
