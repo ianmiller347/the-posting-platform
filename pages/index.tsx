d1c66106bfbc84ca743ec042177032fab1aa0fcd
Ian J. Miller <ianmiller347@gmail.com>
2023-10-24 23:41:55 -0400

    create posts page and add post list basic style (#14)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,12 @@
 import Page from '../components/Page';
-import { PostData } from '../types/post';
-import { useGetPostsQuery } from '../state/post';
-import SkeletonLoaderBars from '../components/SkeletonLoaderBars';
+import PostsList from '../components/PostsList';
 
 export default function Home() {
-  const { data: posts, error, isLoading } = useGetPostsQuery('');
-
   return (
-    <Page title="The Posting Platform" description="">
+    <Page>
       <>
-        <p>Learn more about the posting Platform here</p>
-        {isLoading && <SkeletonLoaderBars />}
-        <ul>
-          {posts?.map((post: PostData) => (
-            <ol key={post.id} className="border mb-2">
-              <h3 className="text-lg">{post.content.titleText}</h3>
-              <p>{post.content.bodyText}</p>
-            </ol>
-          ))}
-        </ul>
-        {error && (
-          <div className="error">Sorry, there was an error loading posts.</div>
-        )}
+        <p className="mb-8">Learn more about the posting Platform here</p>
+        <PostsList />
       </>
     </Page>
   );
